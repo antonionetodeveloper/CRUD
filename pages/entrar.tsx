88e03f0489a31c481c61600c4bc5ab5cd6c3a16d
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2022-10-19 22:58:45 -0300

    fix url's

--- pages/entrar.tsx
+++ pages/entrar.tsx
@@ -18,8 +18,8 @@ export default function Entrar() {
 	async function logIn() {
 		if (checkFields()) {
 			setIsLoading(true)
-			//const url = "https://crud-antonio-neto.vercel.app/"
-			const url = "http://localhost:3000/"
+			const url = "https://crud-antonio-neto.vercel.app/"
+			//const url = "http://localhost:3000/"
 			await axios
 				.post(url + "api/login", {
 					login: login,
