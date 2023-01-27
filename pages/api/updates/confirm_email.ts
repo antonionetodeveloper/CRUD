144e306708e09f629feab382c2607026271168e5
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-01-27 11:35:40 -0300

    back-end feature of confirm email

---
+++ pages/api/updates/confirm_email.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-unsafe-optional-chaining */
+import { ConnectDB } from "./../../../middlewares/conncetDB"
+import { validateTokenJWT } from "./../../../middlewares/validateTokenJWT"
+import { UserModel } from "./../../../models/UserModel"
+import { NextApiResponse } from "next"
+import { NextApiRequest } from "next"
+import nextConnect from "next-connect"
+
+const handler = nextConnect().put(
+	async (req: NextApiRequest, res: NextApiResponse) => {
+		try {
+			const { userId } = req?.query
+			const user = await UserModel.findById(userId)
+			if (!user) {
+				res.status(400).json({ error: "Usuário não encontrado." })
+			}
+
+			const { inputedHashByUser } = req.body
+			if (inputedHashByUser == user.security) {
+				user.verificatedEmail = true
+			} else {
+				return res.status(400).json({ error: "Código incorreto." })
+			}
+
+			await UserModel.findByIdAndUpdate({ _id: user.id }, user)
+			return res.status(200).json({ msg: "Email confirmado com sucesso." })
+		} catch (error) {
+			console.log(error)
+			res.status(400).json({ error: "Usuário não encontrado. " + error })
+		}
+		return res
+			.status(400)
+			.json({ error: "Não foi possivel atualizar o usuário" })
+	},
+)
+export default validateTokenJWT(ConnectDB(handler))
--- pages/configuracoes/confirmar-email.tsx
+++ pages/configuracoes/confirmar-email.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 import { parseCookies } from "nookies"
 import { URL_DEVELOPMENT } from "../_document"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import axios from "axios"
 
 export async function getServerSideProps(context) {
 	const cookies = parseCookies(context)
@@ -13,17 +16,61 @@ export async function getServerSideProps(context) {
 
 	const response = await data.json()
 
+	if (typeof cookies.token == "undefined") {
+		return {
+			props: { data: response, token: null },
+		}
+	}
+
 	return {
-		props: { data: response },
+		props: { data: response, token: cookies.token },
 	}
 }
 
-export default function Page({ data }: any) {
-	console.log("data -> ", data)
+export default function Page({ data, token }: any) {
+	const router = useRouter()
+	const [inputValue, setInputValue] = useState()
+
+	useEffect(() => {
+		if (data.error) {
+			router.push("/entrar")
+		}
+	}, [])
+
+	async function updateInformation() {
+		const reqInstance = axios.create({
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+
+		await reqInstance
+			.put(URL_DEVELOPMENT + "api/updates/confirm_email", {
+				inputedHashByUser: inputValue,
+			})
+			.then(() => {
+				alert("Email confirmado com sucesso.")
+				router.push("/home")
+			})
+			.catch(function (error) {
+				alert(error.response.data.error)
+			})
+	}
 
 	return (
 		<Container>
-			<p>...</p>
+			<input
+				type="text"
+				onChange={(event: any) => setInputValue(event.target.value)}
+			/>
+			<button
+				onClick={() => {
+					//updateInformation()
+					alert("clicou")
+				}}
+			>
+				clique em mim
+			</button>
 		</Container>
 	)
 }
